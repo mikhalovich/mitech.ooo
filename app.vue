cd1840aab4a2eed373e43a4e2f1e2f47ac00f487
blueroose <royalecool460@gmail.com>
2024-01-17 22:33:23 +0300

    Added yandex verification

--- app.vue
+++ app.vue
@@ -10,7 +10,8 @@
 useHead({
   title: 'MITech',
   meta: [
-  { name: 'google-site-verification', content: 'a0_Z6alQ3eFmrUyx_DhTcxX2UOrrdtEsbfvQ-jXKKnE' }
+  { name: 'google-site-verification', content: 'a0_Z6alQ3eFmrUyx_DhTcxX2UOrrdtEsbfvQ-jXKKnE' },
+  { name: 'yandex-verification', content: '4e1fa97fdfbc999e' }
   ]
 });
 </script>
