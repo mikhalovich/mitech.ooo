da4b4b295a664b79de1cbc0823df05474f009c41
blueroose <royalecool460@gmail.com>
2024-01-31 11:07:25 +0300

    Fixed bug with redirect

--- server/middleware/redirect.js
+++ server/middleware/redirect.js
@@ -1,9 +1,11 @@
 export default defineEventHandler((event) => {
   const { req, res } = event.node
   const url = req.url;
-  const index = url.length - 1;
-  if (url[index] === '/') {
-    res.writeHead(301, { Location: url.slice(0, index) });
-    res.end();
+  if (url !== '/') {
+    const index = url.length - 1;
+    if (url[index] === '/') {
+      res.writeHead(301, { Location: url.slice(0, index) });
+      res.end();
+    }
   }
 });
