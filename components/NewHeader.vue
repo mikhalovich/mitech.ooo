907ca47ce7c0207ce428e903596be3ec70152f08
MyNameIsDmitriy <gopanchuk.dm@mail.ru>
2024-09-26 12:36:06 +0300

    fix: unused class

--- components/NewHeader.vue
+++ components/NewHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-screen-desktop-xxl w-full shadow-header">
-    <header class="animate flex items-center gap-12 pt-3 pb-4 px-11" ref="headerRef">
+    <header class="flex items-center gap-12 pt-3 pb-4 px-11" ref="headerRef">
       <NuxtLink to="/" class="text-[32px] leading-[38px] font-normal font-logo text-[#1B1C1E]">MITech</NuxtLink>
       <div class="flex gap-6 text-base font-bold font-logo-secondary text-[#5F6368] [&>*]:cursor-pointer hover:[&>*]:text-[#202124]">
         <NuxtLink to="/" :class="{'text-[#202124]': location === '/'}">Главная страница</NuxtLink>
