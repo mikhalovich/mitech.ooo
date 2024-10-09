ebb0bd53e1c370f98e2e1ba39b623fca8115cadb
MyNameIsDmitriy <gopanchuk.dm@mail.ru>
2024-10-09 11:07:07 +0300

    feat: animated header

--- components/template/Header.vue
+++ components/template/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="max-w-screen-desktop-xxl w-full shadow-header">
-    <header class="flex items-center gap-12 pt-3 pb-4 px-11" ref="headerRef">
+  <div class="w-full shadow-header">
+    <header class="-translate-x-[700px] flex justify-start items-center max-w-screen-desktop-xxl gap-12 m-auto pt-3 pb-4 px-11" ref="headerRef">
       <NuxtLink to="/" class="text-[32px] leading-[38px] font-normal font-logo text-[#1B1C1E]">MITech</NuxtLink>
       <div class="flex gap-6 text-base font-bold font-secondary text-[#5F6368] [&>*]:cursor-pointer hover:[&>*]:text-[#202124]">
         <NuxtLink v-for="link in navLinks" :key="link.path" :to="link.path" :class="{ 'text-[#202124]': location === link.path }">
@@ -28,9 +28,8 @@ const navLinks = [
 ];
 
 onMounted(() => {
-  gsap.fromTo(headerRef.value, 
-    { x: '-100%' }, 
-    { x: '0%', duration: 1.5, ease: "power1.out" },
+  gsap.to(headerRef.value, 
+    { x: '0%', duration: 1.5, ease: "power2.out" },
   );
 });
 </script>
