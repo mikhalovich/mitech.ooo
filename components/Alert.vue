a83b924ae37e13808ff849f4ab571565e1b50c82
Dzmitry Safonau <safonovda1993@gmail.com>
2025-03-06 13:18:42 +0300

    Created by Safonov

--- components/Alert.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="bg-teal-100 border-l-4 border-teal-500 text-teal-900 p-4 fixed bottom-12 right-12 z-[9999] max-tablet:right-4 max-tablet:bottom-4" role="alert">
-    <p>Письмо отправлено на почту. Спасибо!</p>
-  </div>
-</template>
-
-<script setup>
-</script>
